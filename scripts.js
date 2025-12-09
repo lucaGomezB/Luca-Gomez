33291938364dd184ed3dde5207f6af4dca36033b
lucaGomezB <gomez.luca.007@gmail.com>
2025-12-09 19:46:29 -0300

    Mejorada la interacción bilingüe

--- scripts.js
+++ scripts.js
@@ -5,19 +5,19 @@ document.addEventListener('DOMContentLoaded', function() {
     // Define the PDF file based on language
 
     if (userLang.toLowerCase().startsWith('es')) {
-        pdfSource = "LucaValentinoGomezBibiloniCVENGCell.pdf";
-        
-    }else{
-        pdfSource = "LucaValentinoGomezBibiloniCVENGCell.pdf";
+        pdfSource = "LucaValentinoGomezBibiloniCVESCell.pdf";
+        titulo = "Bienvenido a mí página!"
+        // Find the iframe element and update its source
+        const iframe = document.getElementById('pdf-viewer');
+        if (iframe) {
+            iframe.src = pdfSource;
+        }
+        const titulo_pagina = document.getElementById('titulo_pagina');
+        if(titulo_pagina){
+            titulo_pagina.textContent = "Bienvenido a mí página!"
+        }
     }
 
-    // Find the iframe element and update its source
-    const iframe = document.getElementById('pdf-viewer');
-    if (iframe) {
-        iframe.src = pdfSource;
-    }
-    
-    
     // Optional: Update the <html> lang attribute for accessibility/CSS purposes
     document.documentElement.lang = userLang.toLowerCase().startsWith('es') ? 'es' : 'en';
 });
